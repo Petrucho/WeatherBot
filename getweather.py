b5604ddcd4878a01d7ffcdbe91a0e8bd2c7597c3
Petrucho <petrucho@post.cz>
2022-09-05 17:34:00 +0300

    getweather.py, test.py

--- getweather.py
+++ getweather.py
@@ -7,12 +7,14 @@ from config import KEY_WEATHER
 result = getipinfo.get_info(ip.get())[1]
 lat = result.split(',')[0]
 lon = result.split(',')[1]
-print(result.split(','))
-print(f'lat: {lat}')
-print(f'lon: {lon}')
 
-url = "https://api.openweathermap.org/data/2.5/onecall?lat=%s&lon=%s&appid=%s&units=metric" % (lat, lon, KEY_WEATHER)
+# print(result.split(','))
+# print(f'lat: {lat}')
+# print(f'lon: {lon}')
 
+url = "https://api.openweathermap.org/data/2.5/weather?lat=%s&lon=%s&lang=%s&appid=%s&units=metric" % (lat, lon, 'ru', KEY_WEATHER)
 response = requests.get(url)
 data = json.loads(response.text)
-print(data)
+# print(data)
+print(data["main"]["temp"])
+print(data["weather"][0]["description"])
